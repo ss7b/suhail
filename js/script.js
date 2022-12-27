c1b80e4e515f025f608de443d89340a909a63057
suhail <babks93@gmial.com>
2022-12-27 08:53:34 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,99 @@
+// active class on list
+let nav= document.querySelector(".aside-nav");
+let navList = document.querySelectorAll("li")
+let allSection = document.querySelectorAll(".section")
+for (let i = 0; i < navList.length; i++) {
+
+    let a= navList[i].querySelector("a");
+    a.addEventListener('click',function(){
+        removeBakcSection()
+        for (let j = 0; j < navList.length; j++) {
+            if(navList[j].querySelector("a").classList.contains('active')){
+                addBackSection(j)
+            }
+
+            navList[j].querySelector('a').classList.remove("active")
+            
+        }
+
+        this.classList.add("active")
+        showSection(this)
+        if(window.innerWidth < 1100){
+            openAside()
+        }
+    })
+}
+//  back section
+function removeBakcSection(){
+    for (let i = 0; i < allSection.length; i++) {
+        allSection[i].classList.remove("back-section")        
+    }
+}
+function addBackSection(num){
+    allSection[num].classList.add("back-section")
+}
+// ----------show section
+// function showSection(e){
+//     for (let i = 0; i < allSection.length; i++) {
+//         allSection[i].classList.remove("active")        
+//     }
+//     let target= e.getAttribute("href").split("#")[1];
+//     document.querySelector("#" + target).classList.add("active")
+//     console.log(e.getAttribute("href").split("#")[0])
+// }
+function showSection(e){
+    for (let i = 0; i < allSection.length; i++) {
+        allSection[i].classList.remove("active")        
+    }
+    let target= e.getAttribute("href").split("#");
+    let po = target.pop()
+    console.log(po)
+    document.querySelector("#" + po).classList.add("active")
+}
+
+function ubdateNav(e){
+    for (let i = 0; i < navList.length; i++) {
+        navList[i].querySelector("a").classList.remove('active')
+        let target= e.getAttribute("href").split("#")[1];
+        if(target === navList[i].querySelector("a").getAttribute("href").split("#")[1]){
+        navList[i].querySelector("a").classList.add('active')
+        }
+    }
+}
+
+document.querySelector(".hire-me").addEventListener("click",function(){
+    let dataIndex = this.getAttribute("data-section-index")
+    showSection(this)
+    ubdateNav(this)
+    removeBakcSection()
+    addBackSection(dataIndex)
+})
+
+
+
+
+// toggler btn
+
+let togglerBtn = document.querySelector(".nav-toggler "),
+    aside = document.querySelector(".aside");
+
+togglerBtn.addEventListener("click",()=>{
+    openAside()
+})
+function openAside(){
+    aside.classList.toggle("open")
+    togglerBtn.classList.toggle("open")
+    for (let i = 0; i < allSection.length; i++) {
+        allSection[i].classList.toggle("open")        
+    }
+}
+
+// view degree
+let view = document.querySelectorAll(".view");
+let g = document.querySelectorAll(".graduation");
+console.log(g)
+view.forEach(v => {
+    v.addEventListener("click",function(e){
+        e.currentTarget.classList.toggle("active")
+    })
+});
---
+++ js/switcher-style.js
@@ -0,0 +1,58 @@
+// toggler style switvher open class
+let sttingIcon= document.querySelector(".style-switcher-toggler");
+sttingIcon.addEventListener("click", ()=>{
+    document.querySelector(".style-switcher").classList.toggle("open")
+})
+
+// hide style-switcher on scroll
+window.addEventListener("scroll",()=>{
+
+    if(    document.querySelector(".style-switcher").classList.contains("open")){
+
+    document.querySelector(".style-switcher").classList.remove("open")
+
+    }
+})
+
+// them colors 
+let colorsCssLinks = document.querySelectorAll(".alternate-style");
+function setActiveStyle(color){
+    colorsCssLinks.forEach(style => {
+        if(color === style.getAttribute("title")){
+            style.removeAttribute("disabled")
+        }
+        else{
+            style.setAttribute("disabled","true")
+            
+        }
+    });
+}
+
+// them light and dark mode
+let dayNight  = document.querySelector(".day-night");
+let logo = document.querySelector(".logo img")
+console.log(logo.getAttribute("src"))
+
+// toggler class
+dayNight.addEventListener('click',()=>{
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    dayNight.querySelector("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("dark")
+    if(document.body.classList.contains("dark")){
+        logo.setAttribute("src","img/logo-2.PNG")
+    }
+    else{
+        logo.setAttribute("src","img/logo.png")
+    }
+})
+
+window.addEventListener("load",()=>{
+    if(document.body.classList.contains("dark")){
+        dayNight.querySelector("i").classList.add("fa-sun");
+        logo.setAttribute("src","img/logo-2.PNG")
+    }
+    else{
+        dayNight.querySelector("i").classList.add("fa-moon");
+        logo.setAttribute("src","img/logo.png")
+    }
+})
